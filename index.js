4f6f41a8901667bba04be001ffc9c7baeb1da39b
1egend <Charles.ran9@gmail.com>
2021-07-20 22:37:28 -0400

    Temporarily allowed * CORS for local testing (hopefully don't get RCE'd)

--- index.js
+++ index.js
@@ -133,8 +133,8 @@ async function fetchMongoData(club){
 */
 
 http.createServer(function(req, res){
-	res.setHeader('Access-Control-Allow-Origin', 'https://brawltrack.com');
-	// res.setHeader('Access-Control-Allow-Origin', '*');
+	// res.setHeader('Access-Control-Allow-Origin', 'https://brawltrack.com');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Request-Method', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 	res.setHeader('Access-Control-Allow-Headers', '*');

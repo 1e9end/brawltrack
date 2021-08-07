a489b4d3d954e327419189bc958d92870826737c
1egend <Charles.ran9@gmail.com>
2021-08-07 16:08:19 -0400

    Added 4 Mystic Esports clubs

--- clubConfig.mjs
+++ clubConfig.mjs
@@ -4,9 +4,9 @@ dotenv.config();
 export default  {
 	"28GYUQJ9Q": {
 		tag: "28GYUQJ9Q",
-		schedule: '0 */4 * * *',
-		token: process.env.QUOTAGUARDSTATIC_TOKEN,
-		proxy: process.env.QUOTAGUARDSTATIC_URL,
+		schedule: '1 */4 * * *',
+		token: process.env.FIXIE_TOKEN,
+		proxy: process.env.FIXIE_URL,
 	},
 	"C9Y29P8V": {
 		tag: "C9Y29P8V",
@@ -16,26 +16,26 @@ export default  {
 	},
 	"YQ9JYR2Q": {
 		tag: "YQ9JYR2Q",
-		schedule: '2 */8 * * *',
+		schedule: '1 */8 * * *',
 		token: process.env.FIXIE_TOKEN,
 		proxy: process.env.FIXIE_URL,
 	},
 	"2PQLCVJYC": {
 		tag: "2PQLCVJYC",
-		schedule: '3 */8 * * *',
+		schedule: '1 */8 * * *',
 		token: process.env.FIXIE_TOKEN,
 		proxy: process.env.FIXIE_URL,
 	},
 	// Partner
 	"2Q8RLQGJU": {
 		tag: "2Q8RLQGJU",
-		schedule: '4 */8 * * *',
-		token: process.env.FIXIE_TOKEN,
-		proxy: process.env.FIXIE_URL,
+		schedule: '2 */12 * * *',
+		token: process.env.QUOTAGUARDSTATIC_TOKEN,
+		proxy: process.env.QUOTAGUARDSTATIC_URL,
 	},
 	"2LGP82UGV": {
 		tag: "2LGP82UGV",
-		schedule: '5 */8 * * *',
+		schedule: '2 */12 * * *',
 		token: process.env.FIXIE_SOCKS_TOKEN,
 		proxy: process.env.FIXIE_SOCKS_HOST,
 		proxySocks: true,
@@ -43,21 +43,47 @@ export default  {
 	// Ally
 	"28YRVG90P": {
 		tag: "28YRVG90P",
-		schedule: '6 */12 * * *',
+		schedule: '3 */24 * * *',
 		token: process.env.QUOTAGUARDSTATIC_TOKEN,
 		proxy: process.env.QUOTAGUARDSTATIC_URL,
+		proxySocks: true,
 	},
 	"QCUJRG80": {
 		tag: "QCUJRG80",
-		schedule: '7 */12 * * *',
-		token: process.env.FIXIE_TOKEN,
-		proxy: process.env.FIXIE_URL,
+		schedule: '3 */24 * * *',
+		token: process.env.FIXIE_SOCKS_TOKEN,
+		proxy: process.env.FIXIE_SOCKS_HOST,
+		proxySocks: true,
 	},
 	// Mystic Esports
+	"29RC2UR2C": {
+		tag: "29RC2UR2C",
+		schedule: '4 */24 * * *',
+		token: process.env.QUOTAGUARDSTATIC_TOKEN,
+		proxy: process.env.QUOTAGUARDSTATIC_URL,
+	},
 	"2QCJLC89C": {
 		tag: "2QCJLC89C",
-		schedule: '7 */24 * * *',
-		token: process.env.FIXIE_TOKEN,
-		proxy: process.env.FIXIE_URL,
+		schedule: '4 */24 * * *',
+		token: process.env.QUOTAGUARDSTATIC_TOKEN,
+		proxy: process.env.QUOTAGUARDSTATIC_URL,
+	},
+	"29YR888YR": {
+		tag: "29YR888YR",
+		schedule: '4 */24 * * *',
+		token: process.env.QUOTAGUARDSTATIC_TOKEN,
+		proxy: process.env.QUOTAGUARDSTATIC_URL,
+	},
+	"V8R82YRY": {
+		tag: "V8R82YRY",
+		schedule: '4 */24 * * *',
+		token: process.env.QUOTAGUARDSTATIC_TOKEN,
+		proxy: process.env.QUOTAGUARDSTATIC_URL,
+	},
+	"29JYLJ8JL": {
+		tag: "29JYLJ8JL",
+		schedule: '4 */24 * * *',
+		token: process.env.QUOTAGUARDSTATIC_TOKEN,
+		proxy: process.env.QUOTAGUARDSTATIC_URL,
 	}
 };
